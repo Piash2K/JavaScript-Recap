7f6efd7d97c48ef6da3fdf604571337219342d3b
PIASH <zmnobab1999@gmail.com>
2025-11-20 21:08:51 +0600

    recap about string

---
+++ string.js
@@ -0,0 +1,19 @@
+const country = "Bangladesh";
+const division = "Newkhale";
+const district = `B-Baria`;
+// const thana = new String("Demra");
+
+// console.log( thana)
+
+const numbers = [54, 98, 78, 21, 65];
+// console.log(numbers.length);
+// console.log(numbers[0])
+console.log(numbers)
+numbers[1] = 11;
+console.log(numbers)
+const capital = "Dhaka";
+// console.log(capital.length)
+
+console.log(capital);
+ capital[1]="q"   //it will not be changed, cause string is immutable
+ console.log(capital)
