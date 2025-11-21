a882d11cf4622cb472d08fe7f117abdb41710eb7
PIASH <zmnobab1999@gmail.com>
2025-11-21 22:45:16 +0600

    recap introduction to object properties and values

---
+++ object.js
@@ -0,0 +1,26 @@
+//primitive
+const age = 21;
+const school = "rifle square";
+const isPassed = true;
+// let isDeveloper;
+const subjects = ["Bangla", "English", "Physics", "Math"];
+
+//non-primitive
+//object
+const bottle = {
+  brand: "Apple",
+  price: 45,
+  color: "white",
+  isClean: false,
+};
+
+const subject = {
+  name: "biology",
+  teacher: "Rasheda Mam",
+  examDate: "30 Feb",
+  chapters: ["first", "second", "third"],
+  exams: {
+    name: "Final Exam",
+    marks: 100,
+  },
+};
